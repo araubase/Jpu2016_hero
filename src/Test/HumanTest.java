09fae03f0a9289e917cb4629dec4b5c846263ed9
araubase <33415682+araubase@users.noreply.github.com>
2018-05-25 10:16:50 +0200

    Initial commit

---
+++ src/Test/HumanTest.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import jpu2016.hero.Human;
+
+/**
+ * @author Biquet Yoan
+ *
+ */
+public class HumanTest {
+
+	/**
+	 * Test method for {@link jpu2016.hero.Human#Human()}.
+	 */
+	@Test
+	public void testHuman() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ src/Test/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Biquet Yoan
+ *
+ */
+package Test;
